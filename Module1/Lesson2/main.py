011dd24b84af041e99b6ed029cd77e6afbdc5443
bill <truongbill003@gmail.com>
2023-08-08 21:03:58 +0700

    lesson2

---
+++ Module1/Lesson2/main.py
@@ -0,0 +1,80 @@
+# Phương thức 
+'''Không trả về'''
+def sum(a, b):
+    print("Tổng là: {0}".format(a + b))
+    
+sum(3, 7)
+
+
+# Hàm
+'''Có giá trị trả về'''
+def checkbool(boan):
+    if boan is False: return True
+    else: return False
+    
+print("Check bool: {0}".format(checkbool(False)))
+
+# Viết một chương trình Python để đếm số lượng ký tự trong một chuỗi nhập từ người dùng.
+chuoi = input("Nhap vao 1 chuoi ngau nhien: ")
+def xulychuoi(chuoi):
+    result = 0
+    for i in chuoi:
+        result += 1
+    return result
+
+def chuoixuly(chuoi):
+    result = 0
+    for i in chuoi:
+        result += 1
+    print("Chuoi nay dai {} ki tu".format(result))
+    
+chuoixuly(chuoi)
+print(xulychuoi(chuoi), "Ki tu")
+
+# Tạo một danh sách các số nguyên. Viết một phương thức để đếm số lượng số lẻ trong danh sách.
+lst = [123, 333, 55, 677, 123, 3434, 7887, 12321, 5324]
+def demsole(lst):
+    tongsole = 0
+    for i in lst:
+        if i % 2 != 0:
+            tongsole += 1
+    
+    print("Tong cac so le trong danh sach la: {}".format(tongsole))
+
+demsole(lst)
+
+# Cho một danh sách các chuỗi, hãy viết một hàm để đếm số lượng chuỗi có độ dài lớn hơn hoặc bằng 5.
+danhsachnhieuchuoi = ["cohaibao", "mobahongkong", "phiphaisongdaithanhhuyenthoai", "lienquanthangbaitaikynang"]
+def xulydanhsachnhieuchuoi(lst):
+    for i in lst:
+        counter = 0
+        for j in i:
+            counter += 1
+        if counter >= 5:
+            print(i)
+
+xulydanhsachnhieuchuoi(danhsachnhieuchuoi)
+
+# Viết một phương thức để đếm số lượng số chia hết cho 3 trong một dãy số nguyên từ 1 đến N.
+n = int(input("Nhap so nguyen: "))
+
+def demsoluongchiahetcho3(n):
+    total = 0
+    lstchiahetcho3 = []
+    for i in range(1, n + 1):
+        lstchiahetcho3.append(i)
+    for i in range(1, n + 1):
+        if i % 3 == 0:
+            total += 1
+    return total
+
+print(demsoluongchiahetcho3(n))
+
+    
+# Tạo một lớp đại diện cho một hình tròn. Viết các phương thức để tính diện tích và chu vi của hình tròn.
+# Viết một chương trình Python để đếm số lượng từ trong một câu nhập từ người dùng.
+# Tạo một lớp đại diện cho một sách. Viết các phương thức để thêm sách mới, đếm tổng số lượng sách và in ra thông tin về các cuốn sách.
+# Cho một danh sách các số thực, viết một hàm để đếm số lượng số âm trong danh sách.
+# Viết một phương thức Python để kiểm tra xem một chuỗi có phải là chuỗi palindrome hay không.
+# Tạo một lớp đại diện cho một người. Viết các phương thức để nhập thông tin cá nhân, đếm số lượng người đã được tạo và hiển thị thông tin của mỗi người.
+
